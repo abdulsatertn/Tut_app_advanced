7ae3468e679579890087bcf92c1109a3273f0e21
abduustn <115055703+abduustn@users.noreply.github.com>
2024-03-09 12:06:27 -0800

    continue working on routes manager and making a route generator class

--- lib/presentation/resources/routes_manager.dart
+++ lib/presentation/resources/routes_manager.dart
@@ -1,3 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:store_app_advanced/presentation/forget_password/forget_password_view.dart';
+import 'package:store_app_advanced/presentation/login/login_view.dart';
+import 'package:store_app_advanced/presentation/main/main.dart';
+import 'package:store_app_advanced/presentation/register/register_view.dart';
+import 'package:store_app_advanced/presentation/splash/splash_view.dart';
+import 'package:store_app_advanced/presentation/store_details/store_details_view.dart';
+
 class Routes {
   static const String splashRoute = '/';
   static const String loginRoute = '/login';
@@ -5,4 +13,37 @@ class Routes {
   static const String forgetPasswordRoute = '/forgetPassword';
   static const String mainRoute = '/main';
   static const String storeDetailsRoute = '/storeDetails';
+}
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.splashRoute:
+        return MaterialPageRoute(builder: (_) => const SplashView());
+
+      case Routes.loginRoute:
+        return MaterialPageRoute(builder: (_) => const LoginView());
+      case Routes.registerRoute:
+        return MaterialPageRoute(builder: (_) => const RegisterView());
+      case Routes.forgetPasswordRoute:
+        return MaterialPageRoute(builder: (_) => const ForgetPasswordView());
+      case Routes.mainRoute:
+        return MaterialPageRoute(builder: (_) => const MainView());
+      case Routes.storeDetailsRoute:
+        return MaterialPageRoute(builder: (_) => const StoreDetailsView());
+
+      default:
+        return unDefindRoute();
+    }
+  }
+
+  static Route<dynamic> unDefindRoute() {
+    return MaterialPageRoute(
+        builder: (_) => Scaffold(
+              appBar: AppBar(
+                title: const Text('No Route Found'),
+              ),
+              body: const Center(child: Text('No Rout Found')),
+            ));
+  }
 }
