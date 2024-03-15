8df98a6e5bef127f4f2322aa5b1f48f07e369839
abduustn <115055703+abduustn@users.noreply.github.com>
2024-03-15 13:26:05 -0700

    domain layer // creating models

---
+++ lib/data/network/app_api.dart
@@ -0,0 +1,18 @@
+import 'package:retrofit/http.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:store_app_advanced/app/constants.dart';
+import 'package:retrofit_generator/retrofit_generator.dart';
+import 'package:dio/dio.dart';
+import 'package:store_app_advanced/data/response/responses.dart';
+part 'app_api.g.dart';
+
+@RestApi(baseUrl: Constants.baseUrl)
+abstract class AppServiceClient {
+  factory AppServiceClient(Dio dio, {String baseUrl}) = _AppServiceClient;
+
+  @POST('/customers/login')
+  Future<AuthentecationResponse> login(
+      @Field('email') String email, @Field('password') String password);
+}
+
+// to auto generate , in trminal we run 'dart run build_runner build --delete-conflicting-outputs'
--- lib/domain/models.dart
+++ lib/domain/models.dart
@@ -16,3 +16,27 @@ class SliderViewObject {
 
   SliderViewObject(this.sliderObject, this.numberOfSlides, this.currentIndex);
 }
+
+// login models
+class Customer {
+  String id;
+  String name;
+  int numberOfNotifications;
+
+  Customer(this.id, this.name, this.numberOfNotifications);
+}
+
+class Contacts {
+  int phone;
+  String email;
+  int link;
+
+  Contacts(this.phone, this.email, this.link);
+}
+
+class Authentecation {
+  Customer customer;
+  Contacts contacts;
+
+  Authentecation(this.customer, this.contacts);
+}
