98b8e1966194d33ab6764ecbde2e2159c49e7cff
abduustn <115055703+abduustn@users.noreply.github.com>
2024-03-17 08:38:52 -0700

    adding dio logger interceptor

--- lib/data/network/dio_factory.dart
+++ lib/data/network/dio_factory.dart
@@ -1,4 +1,6 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import 'package:store_app_advanced/app/constants.dart';
 
 const String APPLICATION_JSON = 'application/json';
@@ -21,6 +23,16 @@ class DioFactory {
       AUTHORIZATION: 'send token here',
       DEFAULT_LANGUAGE: 'en', // TODO get lang from app prefs
     };
+// so important to use the logger with dio
+    if (!kReleaseMode) {
+      // it is debug mode so  print app logs
+      // start using pretty dio logger package
+      dio.interceptors.add(PrettyDioLogger(
+        requestHeader: true,
+        requestBody: true,
+        responseHeader: true,
+      ));
+    }
 
     dio.options = BaseOptions(
       baseUrl: Constants.baseUrl,
