85311bc9d5b39f0e130db1ab8befc07ba1d6b6a8
abduustn <115055703+abduustn@users.noreply.github.com>
2024-03-19 06:42:11 -0700

    login View UI Add assets, colors, and values managers to LoginView
    Import assets, colors, and values managers in LoginView for future use in UI
    design and styling. Update the build method to return a Scaffold widget and
    add a method to return the content widget with form fields and styling.

--- lib/presentation/login/view/login_view.dart
+++ lib/presentation/login/view/login_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:store_app_advanced/presentation/login/view_model/login_view_model.dart';
+import 'package:store_app_advanced/presentation/resources/assets_manager.dart';
+import 'package:store_app_advanced/presentation/resources/color_manger.dart';
+import 'package:store_app_advanced/presentation/resources/values_manager.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -13,6 +16,7 @@ class _LoginViewState extends State<LoginView> {
 
   final TextEditingController _userNameController = TextEditingController();
   final TextEditingController _userPasswordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   _bind() {
     _viewModel.start(); // tell view model to start ur job
@@ -32,7 +36,46 @@ class _LoginViewState extends State<LoginView> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
+  }
+
+  Widget _getContentWidget() {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.only(
+          top: AppPadding.p100,
+        ),
+        color: ColorManager.white,
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                const Center(
+                  child: Image(
+                    image: AssetImage(ImageAssets.splashLogo),
+                  ),
+                ),
+                const SizedBox(height: AppSize.s28),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: AppPadding.p28, right: AppPadding.p28),
+                  child: StreamBuilder<bool>(
+                    stream: _viewModel.outIsUserNameValid,
+                    builder: (context, snapshot) {
+                      return TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        controller: _userNameController,
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
   @override
