a8b61a1a9b47dd3d142eb38e8c104e9ac56f418e
abduustn <115055703+abduustn@users.noreply.github.com>
2024-03-19 06:42:25 -0700

    Add new padding and size constants
    Added new padding constant p28 and size constant s28 to the values manager
    for increased flexibility in styling.

--- lib/presentation/resources/values_manager.dart
+++ lib/presentation/resources/values_manager.dart
@@ -14,6 +14,8 @@ class AppPadding {
   static const double p16 = 16;
   static const double p18 = 18;
   static const double p20 = 20;
+  static const double p28 = 28;
+  static const double p100 = 100;
 }
 
 class AppSize {
@@ -29,6 +31,8 @@ class AppSize {
   static const double s16 = 16;
   static const double s18 = 18;
   static const double s20 = 20;
+  static const double s28 = 28;
+
   static const double s40 = 40;
   static const double s60 = 60;
   static const double s100 = 100;
