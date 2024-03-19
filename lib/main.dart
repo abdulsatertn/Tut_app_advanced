276702aa5cbae705a8a3aa66031fa6eee331cbfb
abduustn <115055703+abduustn@users.noreply.github.com>
2024-03-19 13:35:50 -0700

    Add async to main function
    Make the main function asynchronous to ensure all necessary functions are
    ready before running the app. Also, initialize the app module before running
    the app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:store_app_advanced/app/app.dart';
+import 'package:store_app_advanced/app/di.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding
+      .ensureInitialized(); // to make sure that the app wont run before all aways funs are ready
+  await initAppModule();
   runApp(const MyApp());
 }
