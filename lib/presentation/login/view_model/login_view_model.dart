b64cd95d20b780017b49bb44bc254c7d6017f09c
abduustn <115055703+abduustn@users.noreply.github.com>
2024-03-19 04:17:11 -0700

    Add LoginViewModel with inputs and outputs
    Implement LoginViewModel with input and output methods for setting username and password, as well as login functionality. Also includes streams for validating username and password.

--- lib/presentation/login/view_model/login_view_model.dart
+++ lib/presentation/login/view_model/login_view_model.dart
@@ -1,1 +1,63 @@
+import 'package:store_app_advanced/presentation/base/base_view_model.dart';
 
+class LoginViewModel extends LoginViewModelInputs with LoginViewModelOutputs {
+  // inputs
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+
+  @override
+  void start() {
+    // TODO: implement start
+  }
+
+  @override
+  // TODO: implement inputPassword
+  Sink get inputPassword => throw UnimplementedError();
+
+  @override
+  // TODO: implement inputUserName
+  Sink get inputUserName => throw UnimplementedError();
+
+  @override
+  setPassword(String password) {
+    // TODO: implement setPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  setUserName(String userName) {
+    // TODO: implement setUserName
+    throw UnimplementedError();
+  }
+
+  @override
+  login() {
+    // TODO: implement login
+    throw UnimplementedError();
+  }
+
+  // Outputs
+  @override
+  // TODO: implement outIsPasswordValid
+  Stream<bool> get outIsPasswordValid => throw UnimplementedError();
+
+  @override
+  // TODO: implement outIsUserNameValid
+  Stream<bool> get outIsUserNameValid => throw UnimplementedError();
+}
+
+abstract class LoginViewModelInputs extends BaseViewModel {
+  setUserName(String userName);
+  setPassword(String password);
+  login();
+
+  Sink get inputUserName;
+  Sink get inputPassword;
+}
+
+abstract mixin class LoginViewModelOutputs {
+  Stream<bool> get outIsUserNameValid;
+  Stream<bool> get outIsPasswordValid;
+}
