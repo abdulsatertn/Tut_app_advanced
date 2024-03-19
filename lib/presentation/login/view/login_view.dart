10f4ec294bfcd54c894e56113b68ba8aea387ebf
abduustn <115055703+abduustn@users.noreply.github.com>
2024-03-19 06:12:43 -0700

    Add LoginViewModel and bind methods to LoginView
    Introduce LoginViewModel to LoginView and bind text controllers to view model
    methods for updating user name and password. Also implement dispose method.

--- lib/presentation/login/view/login_view.dart
+++ lib/presentation/login/view/login_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:store_app_advanced/presentation/login/view_model/login_view_model.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -8,8 +9,35 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  final LoginViewModel _viewModel = LoginViewModel(_loginUseCase);
+
+  final TextEditingController _userNameController = TextEditingController();
+  final TextEditingController _userPasswordController = TextEditingController();
+
+  _bind() {
+    _viewModel.start(); // tell view model to start ur job
+
+    _userNameController
+        .addListener(() => _viewModel.setUserName(_userNameController.text));
+
+    _userPasswordController.addListener(
+        () => _viewModel.setPassword(_userPasswordController.text));
+  }
+
+  @override
+  void initState() {
+    _bind();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
+  }
+
+  @override
+  void dispose() {
+    _viewModel.dispose();
+    super.dispose();
   }
 }
