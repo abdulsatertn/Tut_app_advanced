9a2e2e99aa270236f40a81d900b90d4f971f9174
abduustn <115055703+abduustn@users.noreply.github.com>
2024-03-21 12:50:19 -0700

    Refactor StateRenderer build method
    Refactor StateRenderer build method to return a Container and add helper
    methods for handling different stateRendererTypes.

--- lib/presentation/common/state_renderer/state_renderer.dart
+++ lib/presentation/common/state_renderer/state_renderer.dart
@@ -41,6 +41,31 @@ class StateRenderer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container();
+  }
+
+  Widget _getStateWidget() {
+    switch (stateRendererType) {
+      case StateRendererType.popupLoadingState:
+      // TODO: Handle this case.
+      case StateRendererType.popupErrorState:
+      // TODO: Handle this case.
+      case StateRendererType.fullScreenLoadingState:
+      // TODO: Handle this case.
+      case StateRendererType.fullScreenErrorState:
+      // TODO: Handle this case.
+      case StateRendererType.fullScreenEmptyState:
+      // TODO: Handle this case.
+      case StateRendererType.contentState:
+      // TODO: Handle this case.
+    }
+  }
+
+  Widget _getItemsColumn(List<Widget> children) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: children,
+    );
   }
 }
